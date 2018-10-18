455c45f9e6929d6706aea3e0e62955dc5ba823c2
Jesse Tiu <jessetiu@gmail.com>
2018-10-18 21:47:38 +1300

    Make header and footer with nice formatting and working links to a few pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-	root 'welcome#index'
+
+  # Page format:
+  # get '/url', to: 'controller#action'
+  # I think the default action is go to the view.
+  # To link to a page, use <url>_path
+  get '/signup', to: 'users#signup'
+  get '/login', to: 'users#login'
+  get 'users/new'
+  get 'sessions/new'
+	root 'users#loginmenu'
+
+  # In future
+
+  # For edit details page, url should be the username
+  
+  # get '/timesheet', to: 'timesheet#form' # or something similar
+  # post '/timesheet', to: ???
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
